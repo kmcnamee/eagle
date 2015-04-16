925241f00c0e5757062326f9d8911881030c78ed
kmcnamee <kmcnamee@gmail.com>
2015-04-16 17:47:34 -0400

    initial

---
+++ RestSample/Scripts/app/login.controller.js
@@ -0,0 +1,26 @@
+﻿eagleRest.controller('LoginCtrl',
+    ['$scope', '$http', '$location','AuthenticationService',
+    function ($scope, $http, $location, AuthenticationService) {
+
+        AuthenticationService.ClearCredentials();
+
+        $scope.username = "";
+        $scope.password = "";
+        $scope.sessionId = "";
+
+        $scope.login = function () {
+            $scope.dataLoading = true;
+            AuthenticationService.login($scope.username, $scope.password, function(response){
+                if (response.success) {                    
+                    AuthenticationService.SetCredentials($scope.username, $scope.password,response.sessionId);
+                    $location.path('/holding');
+                }
+                else {
+                    console.log(response);
+                    $scope.error = response.message;                    
+                    $scope.dataLoading = false;
+                }
+            })
+        }
+        
+    }]);
---
+++ RestSample/Scripts/app/services.js
@@ -0,0 +1,41 @@
+﻿'use strict';
+
+angular.module('authentication', [])
+
+.factory('AuthenticationService',
+    ['$http', '$cookieStore', '$rootScope',
+    function ($http, $cookieStore, $rootScope) {
+        var service = {};
+
+        service.login = function (username, password, callback) {
+
+            /* Use this for real authentication
+             ----------------------------------------------*/
+            $http.post('http://localhost/eagle.rest/api/authenticate', { username: username, password: password })
+                .success(function (response) {
+                    callback(response);
+                });
+
+        };
+
+        service.SetCredentials = function (username, password, sessionId) {
+                        
+            $rootScope.globals = {
+                currentUser: {
+                    username: username,
+                    sessionId: sessionId
+                }
+            };
+
+            $http.defaults.headers.common['EagleSessionID'] = sessionId; // jshint ignore:line
+            $cookieStore.put('globals', $rootScope.globals);
+        };
+
+        service.ClearCredentials = function () {
+            $rootScope.globals = {};
+            $cookieStore.remove('globals');
+            $http.defaults.headers.common['EagleSessionID'] = ' ';
+        };
+
+        return service;
+    }]);
